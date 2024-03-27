55bd8e18a1c32b21df33eb16c95f8f1076ca4990
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2024-03-27 23:31:26 +0300

    I tried to save layer with GeoJson but I couldnt this time

--- MoskPharmacy/Controllers/MapController.cs
+++ MoskPharmacy/Controllers/MapController.cs
@@ -1,10 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MoskPharmacy.Context;
+using MoskPharmacy.Models;
+using MoskPharmacy.ViewModels;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MoskPharmacy.Controllers;
 public class MapController : Controller
 {
+    private readonly MoskPharmacyContext _context;
+
+    public MapController(MoskPharmacyContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult SaveGeoJson([FromBody] JObject geoJsonData)
+    {
+        var values = JsonConvert.DeserializeObject<List<DrawingViewModel>>(geoJsonData.ToString());
+        
+        
         return View();
     }
 }
